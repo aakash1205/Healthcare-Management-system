1bbb3819064e40d645b9f3f718b5ba970edf2ed0
aakash1205 <114174835+aakash1205@users.noreply.github.com>
2024-11-09 20:45:35 +0530

    added logins

--- frontend/src/pages/Login/PatientLogin.jsx
+++ frontend/src/pages/Login/PatientLogin.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const PatientLogin = () => (
   <div className="flex justify-center items-center min-h-screen bg-gray-100">
     <div className="w-full max-w-md bg-white p-6 rounded shadow-lg">
-      <h2 className="text-2xl font-bold mb-6 text-center">Admin Login</h2>
+      <h2 className="text-2xl font-bold mb-6 text-center">Patient Login</h2>
       <form>
         <input type="text" placeholder="Email" className="w-full px-4 py-2 mb-4 border rounded"/>
         <input type="password" placeholder="Password" className="w-full px-4 py-2 mb-6 border rounded"/>
